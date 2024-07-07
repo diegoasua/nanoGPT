ba88a5fe55ffe5fae66ea5b19a49350d20a88397
diegoasua <baxuaa@gmail.com>
2024-07-07 20:35:33 +0700

    experiments

--- config/train_wiki.py
+++ config/train_wiki.py
@@ -2,7 +2,7 @@
 # good for debugging and playing on macbooks and such
 
 out_dir = 'out-wiki'
-eval_interval = 25 # keep frequent because we'll overfit
+eval_interval = 20 # keep frequent because we'll overfit
 eval_iters = 20
 log_interval = 10 # don't print too too often
 
@@ -15,22 +15,23 @@ wandb_run_name = 'mini-gpt-wiki'
 
 dataset = 'wiki'
 gradient_accumulation_steps = 1
-batch_size = 1 # batch size of 6 and block size of 1024 worked
-block_size = 4096 # context of up to 1024 previous tokens
+batch_size = 8 # batch size
+block_size = 512 # context of up to 4096 previous tokens
 
 # baby GPT model :)
-n_layer = 6
-n_head = 8
-n_embd = 512
+n_layer = 3
+n_head = 6
+n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 200
-lr_decay_iters = 200 # make equal to max_iters usually
+max_iters = 1_000_000_000
+lr_decay_iters = max_iters # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 10 # not super necessary potentially
+device = 'mps'
 
 # on macbook also add
 # device = 'cpu'  # run on cpu only
--- data/wiki/prepare.py
+++ data/wiki/prepare.py
@@ -1,32 +1,71 @@
 import os
-import requests
 import tiktoken
 import numpy as np
 
-# download the tiny shakespeare dataset
+def process_file_in_chunks(input_file_path, train_output_path, val_output_path, chunk_size=500*1024*1024):  # 500 MB chunks
+    enc = tiktoken.get_encoding("gpt2")
+    file_size = os.path.getsize(input_file_path)
+    split_point = int(file_size * 0.9)
+    
+    with open(input_file_path, 'r', encoding='utf-8') as f, \
+         open(train_output_path, 'wb') as train_out, \
+         open(val_output_path, 'wb') as val_out:
+        
+        bytes_read = 0
+        train_tokens = 0
+        val_tokens = 0
+        
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            
+            chunk_bytes = chunk.encode('utf-8')
+            chunk_size = len(chunk_bytes)
+            
+            if bytes_read + chunk_size <= split_point:
+                # This chunk belongs entirely to the training set
+                ids = enc.encode_ordinary(chunk)
+                train_tokens += len(ids)
+                np.array(ids, dtype=np.uint16).tofile(train_out)
+            elif bytes_read < split_point < bytes_read + chunk_size:
+                # This chunk needs to be split between train and val
+                split_index = split_point - bytes_read
+                train_part = chunk[:split_index]
+                val_part = chunk[split_index:]
+                
+                train_ids = enc.encode_ordinary(train_part)
+                val_ids = enc.encode_ordinary(val_part)
+                
+                train_tokens += len(train_ids)
+                val_tokens += len(val_ids)
+                
+                np.array(train_ids, dtype=np.uint16).tofile(train_out)
+                np.array(val_ids, dtype=np.uint16).tofile(val_out)
+            else:
+                # This chunk belongs entirely to the validation set
+                ids = enc.encode_ordinary(chunk)
+                val_tokens += len(ids)
+                np.array(ids, dtype=np.uint16).tofile(val_out)
+            
+            bytes_read += chunk_size
+    
+    return train_tokens, val_tokens
+
+# Paths
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+train_output_path = os.path.join(os.path.dirname(__file__), 'train.bin')
+val_output_path = os.path.join(os.path.dirname(__file__), 'val.bin')
+
 if not os.path.exists(input_file_path):
-    print("training data not found")
+    print("Training data not found")
     exit()
 
-with open(input_file_path, 'r', encoding='utf-8') as f:
-    data = f.read()
-n = len(data)
-train_data = data[:int(n*0.9)]
-val_data = data[int(n*0.9):]
+# Process the file
+train_tokens, val_tokens = process_file_in_chunks(input_file_path, train_output_path, val_output_path)
 
-# encode with tiktoken gpt2 bpe
-enc = tiktoken.get_encoding("gpt2")
-train_ids = enc.encode_ordinary(train_data)
-val_ids = enc.encode_ordinary(val_data)
-print(f"train has {len(train_ids):,} tokens")
-print(f"val has {len(val_ids):,} tokens")
-
-# export to bin files
-train_ids = np.array(train_ids, dtype=np.uint16)
-val_ids = np.array(val_ids, dtype=np.uint16)
-train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
-val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+print(f"Train has {train_tokens:,} tokens")
+print(f"Val has {val_tokens:,} tokens")
 
 # train.bin has 301,966 tokens
 # val.bin has 36,059 tokens
